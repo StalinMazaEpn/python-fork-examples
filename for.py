47fdbe1f0dd4c185a2503771308c2ad25fe784ec
Stalin <stalinct97@gmail.com>
2026-02-07 22:50:17 -0500

    simulate error at for.py file

--- for.py
+++ for.py
@@ -1,3 +1,11 @@
+password="admin123"   # Bandit B105
+
+def login():
+    print("login")
+
+defregister():         # error de estilo + tabulación
+    print("register")
+
 list_ = [5, 14, 28, 30, 26, 45]
 
 for i in list(filter(lambda x:x>29, list_)):
